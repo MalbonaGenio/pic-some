3e3843c5ce1a198b541d17381cf28111207d647b
Pau <pvalverdemolina@gmail.com>
2021-04-24 07:26:57 +0900

    fetched the pic data and added to the provider. set up a context provider. goignt o be used for the pictures component.

--- pic-some/src/index.js
+++ pic-some/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import {ContextProvider} from "./Context.js"
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
-  document.getElementById('root')
-);
+  <ContextProvider>
+      <Router>
+          <App />
+      </Router>
+  </ContextProvider>, 
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
