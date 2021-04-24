07d0a76470e3b33ae2aab1aebe74bfb9abe4c35b
Pau <pvalverdemolina@gmail.com>
2021-04-25 07:51:38 +0900

    Added new logic with a fucntion to dsplay a filled heart when isfavorite or a empty one on hover.
    Also added typecheking for the Image component

--- pic-some/src/Context.js
+++ pic-some/src/Context.js
@@ -13,7 +13,6 @@ function ContextProvider({children}) {
     function toggleFavorite(id) {
         const updatedArr = allPhotos.map(photo => {
             if(photo.id === id) {
-                console.log(!photo.isFavorite)
                 return{
                     ...photo,
                     isFavorite: !photo.isFavorite
--- pic-some/src/components/Image.js
+++ pic-some/src/components/Image.js
@@ -1,9 +1,22 @@
 import React, { useState, useContext } from "react"
+import PropType from "prop-types"
 import { Context } from "../Context"
 
 function Image({img, className}) {
     const [hovered, setHovered] = useState(false)
     const {toggleFavorite} = useContext(Context)
+
+    function heartIcon() {
+        if(img.isFavorite) {
+            return <i 
+            className="ri-heart-fill favorite" 
+            onClick={() => toggleFavorite(img.id)}></i>
+        } else if(hovered) {
+            return <i 
+            className="ri-heart-line favorite" 
+            onClick={() => toggleFavorite(img.id)}></i>
+        }
+    }
 
     return (
         //sets image-container but allows to pass things trough className too.
@@ -12,13 +25,17 @@ function Image({img, className}) {
             onMouseEnter={() => setHovered(true)}
             onMouseLeave={() => setHovered(false)}>
             <img src={img.url} className="image-grid"/>
-            {hovered && <i 
-                className="ri-heart-line favorite" 
-                onClick={() => toggleFavorite(img.id)}></i>
-            }
+            {heartIcon()}
             {hovered && <i className="ri-add-circle-line cart"></i>}
         </div>
     )
 }
-
+Image.propType = {
+    className: PropType.string,
+    img: PropType.shape({
+        id: PropType.string.isRequired,
+        url: PropType.string.isRequired,
+        isFavorite: PropType.bool
+    })
+}
 export default Image
