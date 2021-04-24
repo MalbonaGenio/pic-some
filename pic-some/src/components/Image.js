6ee40e01358293e111f3c560aa934aed275420a6
Pau <pvalverdemolina@gmail.com>
2021-04-24 09:49:31 +0900

    added event listeners when mouse enters and leaves an image component and stored as a bolean in state. Will use this to show the fav and add to cart icons later on.

--- pic-some/src/components/Image.js
+++ pic-some/src/components/Image.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 
 function Image({img, className}) {
+    const [hovered, setHovered] = useState(false)
     return (
         //sets image-container but allows to pass things trough className too.
-        <div className={`${className} image-container`}>
+        <div 
+            className={`${className} image-container`} 
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}>
             <img src={img.url} className="image-grid"/>
         </div>
     )
